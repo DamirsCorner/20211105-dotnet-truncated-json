a045c2767d77615f074b1996e44d40cd9efeb06e
Damir Arh <damir.arh@gmail.com>
2021-10-20 13:53:46 +0200

    Sample controller

---
+++ TruncatedJson/Controllers/SampleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruncatedJson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return this.GetStrings();
+        }
+
+        [HttpGet]
+        [Route("materialized")]
+        public IEnumerable<string> GetMaterialized()
+        {
+            return this.GetStrings().ToList();
+        }
+
+        private IEnumerable<string> GetStrings()
+        {
+            foreach (var i in Enumerable.Range(1, 1500))
+            {
+                yield return $"Item {i}";
+            }
+
+            throw new Exception();
+        }
+    }
+}
